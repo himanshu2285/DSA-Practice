7e21b4c1b02a2ab6a1beedf21b8494a1f69d05dc
himanshu2285 <himanshu2k05m@gmail.com>
2025-11-15 23:13:20 +0530

    Added some more pyramid star patterns

--- patterns/pat8.py
+++ patterns/pat8.py
@@ -11,7 +11,7 @@ formula: space= n-i-1, star=2*i+1
 
 
 num = 5
-for i in range(num):  # 0 1 2 3 4
+for i in range(num-1, -1, -1):  # 4 3 2 1 0
     # Space 
     for j in range(num - i - 1):
         print(" ", end=" ")
