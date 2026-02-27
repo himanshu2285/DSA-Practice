b1831b991a181e5d2fc72386d9633abc830b8e3c
himanshu2285 <himanshu2k05m@gmail.com>
2026-02-27 23:59:52 +0530

    Updated code for reverse integer to avoid error with negative numbers

--- Basics/reverse_integer.py
+++ Basics/reverse_integer.py
@@ -1,11 +1,32 @@
+# 1st approach (Not work well for negative numbers, because python keeps remainder positive for negative numbers)
+
+# class Solution:
+#     def reverse(self, x: int) -> int:
+#         rev = 0
+#         while(x!=0):
+#             rem = x % 10
+#             rev = rev * 10 + rem
+#             x = x // 10
+#         return rev
+    
+# obj = Solution()
+# print(obj.reverse(-1230))
+
+
+# 2nd Approuch
 class Solution:
     def reverse(self, x: int) -> int:
         rev = 0
-        while(x!=0):
+        sign = 1 if x >= 0 else -1
+        x = abs(x)
+        
+        while x != 0:
             rem = x % 10
             rev = rev * 10 + rem
-            x = x // 10
-        return rev
+            x //= 10
+        
+        return rev * sign
     
 obj = Solution()
-print(obj.reverse(123))
+print(obj.reverse(-1230))
+    
