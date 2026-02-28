c21e7ea6432b2c1bb4522bce3a0a02c11cf69697
himanshu2285 <himanshu2k05m@gmail.com>
2026-02-28 16:26:26 +0530

    Armstrong Number, Print all the divisors of a number

--- Basics/basic_maths.py
+++ Basics/basic_maths.py
@@ -46,3 +46,50 @@ class Solution_Palindrome:
 obj = Solution_Palindrome()
 print(obj.isPalindrome(10))
         
+
+# 4. Armstrong Number
+class Solution_Armstrong:
+    def isArmstrong(self, n: int) -> bool:
+        # sign = 1 if n>=0 else -1
+        orig = n
+        sign = '-' if n < 0 else ''
+        n = abs(n)
+        sum = 0
+        while(n>0):
+            rem = n % 10
+            sum += rem ** 3
+            n //= 10
+        return sum == orig if sign == '' else False
+obj = Solution_Armstrong()
+print(obj.isArmstrong(0))
+
+
+
+# 5. Print all the divisors of a number
+# 1st approach
+class Solution_Divisors:
+    def printDivisors(self, n: int) -> list[int]:
+        divisors = []
+        for i in range(1, n+1):
+            if n % i == 0:
+                divisors.append(i)
+        return divisors
+obj = Solution_Divisors()
+# print(obj.printDivisors(78))
+# This code's time is O(n) because we are iterating from 1 to n to find the divisors.
+
+# 2nd approach - Optimized
+from math import sqrt
+class Solution_Divisors2:
+    def printDivisors(self, n: int) -> list[int]:
+        divisors = []
+        for i in range(1, int(sqrt(n))+1):
+            if n % i == 0:
+                divisors.append(i)
+                if n // i != i:  # To avoid adding the square root twice for perfect squares
+                    divisors.append(n // i)
+        divisors.sort()  # Sort the divisors in ascending order
+        return divisors
+obj = Solution_Divisors2()
+print(obj.printDivisors(36))
+# Time Complexity = O(sqrt(N))
