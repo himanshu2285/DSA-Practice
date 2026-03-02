8e26193bf564276553271a0bc7492b84d2b0a4b5
himanshu2285 <himanshu2k05m@gmail.com>
2026-03-03 00:05:39 +0530

    Add Printing of numbers till N, and Sum of N numbers

--- Basics/basic_recursion.py
+++ Basics/basic_recursion.py
@@ -6,6 +6,36 @@ class Solution:
         print(name)
         self.printName(name, n-1)
 obj = Solution()
-obj.printName("Alice", 5)
+obj.printName("Alice", 2)
 
 
+# 2. Print 1 to N using Recursion
+class Solution_Print:
+    def printOneToN(self, n: int) -> None:
+        if n <= 0:
+            return
+        self.printOneToN(n-1)
+        print(n)
+obj = Solution_Print()
+obj.printOneToN(5)
+print()
+
+# 3. Print N to 1 using Recursion
+class Solution_Print:
+    def printNum(self, n: int) -> None:
+        if n <= 0:
+            return
+        print(n)
+        self.printNum(n-1)
+obj = Solution_Print()
+obj.printNum(5)
+print()
+
+# 4. Sum of first N natural numbers using Recursion
+class Solution_Sum:
+    def fun(self, num:int, sum:int) -> int:
+        if num <= 0:
+            return sum
+        return self.fun(num-1, sum+num)
+obj = Solution_Sum()
+print(obj.fun(100, 0))
