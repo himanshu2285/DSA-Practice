b2fd7d396e37a744ef06b64e2ab756aa89be0dc1
himanshu2285 <himanshu2k05m@gmail.com>
2026-03-17 23:57:43 +0530

    Add frequency hashing implementation with query processing

---
+++ Hashing.py
@@ -0,0 +1,22 @@
+# Read size of array
+n = int(input())
+
+# Read array elements
+arr = list(map(int, input().split()))
+
+# Precompute frequency (hash array)
+hash_arr = [0] * 13
+
+for i in range(n):
+    hash_arr[arr[i]] += 1
+
+# Read number of queries
+q = int(input())
+print("Query results:")
+
+# Process each query
+while q > 0:
+    number = int(input())
+    print(hash_arr[number])
+    q -= 1
+    
