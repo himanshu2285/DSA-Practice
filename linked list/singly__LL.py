df83f4891a74d2c7e57aeb7620c06510fb901a97
himanshu2285 <himanshu2k05m@gmail.com>
2026-07-02 00:28:05 +0530

    Linked List: Insertion and Deletion

--- linked list/singly__LL.py
+++ linked list/singly__LL.py
@@ -29,10 +29,55 @@ class Singly_linkedList:
                 print(curr_node.val, end=" ")
                 curr_node=curr_node.next
             print()
+            
+    def insert_at(self, val, position):
+        new_node=Node(val)
+        if position==0:
+            new_node.next=self.head
+            self.head=new_node
+        else:
+            curr_node=self.head
+            prev_node=None
+            count=0
+            while curr_node is not None and count<position:
+                prev_node=curr_node
+                curr_node=curr_node.next
+                count+=1
+            prev_node.next=new_node
+            new_node.next=curr_node
+            
+    def delete(self, val):
+        curr_node=self.head
+        # if we have to delete first value
+        if curr_node.next is not None:
+            if curr_node.val==val:
+                self.head=curr_node.next
+                return
+            else:
+                found=False
+                prev_node=None
+                while curr_node is not None:
+                    if curr_node.val==val:
+                        found=True
+                        break
+                    prev_node=curr_node
+                    curr_node=curr_node.next
+                
+                if found:
+                    prev_node.next=curr_node.next
+                    return 
+                else:
+                    print('Node not found')
 
 sll = Singly_linkedList()
 sll.append(10)
 sll.append(20)
 sll.append(30)
-sll.append(40)
+sll.append(35)
+# sll.insert_at(35, 3)
+# sll.insert_at(5, 0)
+sll.insert_at(40, 4)
+sll.traverse()
+
+sll.delete(35)
 sll.traverse()
